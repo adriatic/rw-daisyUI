a37a3c790ab3149f6be48edf08a37a4c168f6e6d
nik <nik@Nikolajs-MacBook-Pro.local>
2023-02-28 17:34:58 -0500

    Home & About pages

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>Home Page</h1>
+    </>
+  )
+}
+
+export default HomePage
